7b5ebf41ecd7ca1a6326684c207f2cfffcd10a64
Daniele Montecchi <montecchi.daniele@gmail.com>
2021-12-06 15:11:58 +0100

    wip

--- assets/laravel-geo/update-cities.sh
+++ assets/laravel-geo/update-cities.sh
@@ -1,6 +1,9 @@
 cd data
 wget https://www.istat.it/storage/codici-unita-amministrative/Elenco-comuni-italiani.csv it-cities.csv
 mv Elenco-comuni-italiani.csv it-cities.csv
+cd ..
+cd ..
+cd ..
 git add .
 git commit -m "[laravel geo] Aggiornato il csv delle città"
-git push
+git push origin
